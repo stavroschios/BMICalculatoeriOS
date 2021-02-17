c7af40ddb7d9c14ba8f6a0457714b79857ca227f
Stavros <stavroschios@gmail.com>
2021-02-17 10:31:08 -0500

    first commit

---
+++ BMI Calculator/Models/BMI.swift
@@ -0,0 +1,15 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Stavros on 18/02/2021.
+//  Copyright © 2021. All rights reserved.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
